0c837d613c7e750c7ff67d82e704b02b8a8fbd60
Chitraksh Tarun <65770420+ChitrakshTarun@users.noreply.github.com>
2024-08-29 21:45:21 +0530

    feat(api): add route for liveruns

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,13 +1,24 @@
-import { ScrollView } from "react-native";
 import { ThemedText, ThemedView } from "@/components/ThemedComponents";
+import liveruns from "@/data/liveruns.json";
+import { useQuery } from "@tanstack/react-query";
+import { FlatList } from "react-native";
 
 const HomePage = () => {
+  // const { data: liveruns, isLoading } = useQuery({
+  //   queryKey: ["liveruns"],
+  //   queryFn: () => fetch("/api/liveruns").then((res) => res.json()),
+  //   refetchInterval: 1000,
+  // });
+  // if (isLoading) return <ThemedText>Loading...</ThemedText>;
+
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <ThemedView>
-        <ThemedText>PaceMan</ThemedText>
-      </ThemedView>
-    </ScrollView>
+    <ThemedView style={{ alignItems: "center", width: "100%", flex: 1 }}>
+      <FlatList
+        data={liveruns}
+        keyExtractor={(item: any) => item.worldId}
+        renderItem={({ item }) => <ThemedText>{item.nickname}</ThemedText>}
+      />
+    </ThemedView>
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,30 @@
 import { Stack } from "expo-router";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
