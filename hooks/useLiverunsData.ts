312719d5fa0e0b591cde1acd6e42fd2d8bef663e
Fevenir <65770420+ChitrakshTarun@users.noreply.github.com>
2024-11-13 14:36:49 +0530

    refactor(api): directly add urls into query hooks
    need to figure out if i even need api routes right now or not

--- hooks/useLiverunsData.ts
+++ hooks/useLiverunsData.ts
@@ -1,44 +1,23 @@
-// hooks/useLiverunsData.ts
 import { useQuery } from "@tanstack/react-query";
-import { useFocusEffect } from "expo-router";
-import { useCallback, useRef, useEffect } from "react";
+import { apiToPace, paceSort } from "@/lib/utils/converters";
 
 interface LiverunsDataParams {
   gameVersion?: string;
   liveOnly?: boolean;
 }
 
-export const useLiverunsData = ({ gameVersion, liveOnly }: LiverunsDataParams) => {
-  const isFocusedRef = useRef(true);
-
-  useFocusEffect(
-    useCallback(() => {
-      isFocusedRef.current = true;
-      return () => {
-        isFocusedRef.current = false;
-      };
-    }, [])
-  );
-
-  const { data, refetch, ...rest } = useQuery({
-    queryKey: ["liveruns", gameVersion, liveOnly],
+export function useLiverunsData({ gameVersion, liveOnly }: LiverunsDataParams) {
+  return useQuery({
+    queryKey: ["liveruns"],
     queryFn: async () => {
-      const response = await fetch(`/api/liveruns?gameVersion=${gameVersion}&liveOnly=${liveOnly ? "true" : "false"}`);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
+      const response = await fetch(
+        `https://paceman.gg/api/ars/liveruns?gameVersion=${gameVersion}&liveOnly=${liveOnly}`
+      );
+      const data = await response.json();
+      const res = (await apiToPace(data)).sort(paceSort);
+      return res;
     },
-    enabled: true,
-    refetchInterval: isFocusedRef.current ? 10000 : false,
-    refetchOnWindowFocus: "always",
-    refetchOnReconnect: "always",
-    staleTime: 10000,
+    refetchInterval: 5000,
+    staleTime: 5000,
   });
-
-  useEffect(() => {
-    refetch();
-  }, [gameVersion, liveOnly, refetch]);
-
-  return { data, refetch, ...rest };
-};
+}
