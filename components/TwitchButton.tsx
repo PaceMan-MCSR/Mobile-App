3416ad667397d6da38eb6e50b9480f18afc286d8
Fevenir <65770420+ChitrakshTarun@users.noreply.github.com>
2025-02-08 14:55:06 +0530

    fix(ui): fix twitch button bg color

--- components/TwitchButton.tsx
+++ components/TwitchButton.tsx
@@ -7,7 +7,7 @@ const TwitchButton = ({ href }: { href: string }) => {
       onPress={() => {
         Linking.openURL(`https://twitch.tv/${href}`);
       }}
-      className={`flex flex-row items-center p-3 gap-2 rounded-xl bg-[${`#9146FF`}]`}
+      className={`flex flex-row items-center p-3 gap-2 rounded-xl bg-[#9146FF]`}
     >
       <Text numberOfLines={1} className="text-white text-lg font-bold">
         {href ?? "Stream"}
