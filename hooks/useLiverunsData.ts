b2d37362fba24d585b769229f1002a4db45b2c86
chitrakshtarun <65770420+chitrakshtarun@users.noreply.github.com>
2025-09-14 01:57:05 +0530

    feat(api): condition useLiveruns to only refetch while page is focused

--- hooks/useLiverunsData.ts
+++ hooks/useLiverunsData.ts
@@ -1,9 +1,29 @@
 import { Pace, PaceSettings } from "@/lib/types/Pace";
 import { apiToPace, paceSort } from "@/lib/utils/converters";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { usePathname } from "expo-router";
 
 export const useLiverunsData = ({ gameVersion, liveOnly }: PaceSettings) => {
+  const pathname = usePathname();
+  const isFocusedOnHomePage = pathname === "/";
   return useQuery<Pace[]>({
+    queryKey: ["liveruns", gameVersion, liveOnly],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://paceman.gg/api/ars/liveruns?gameVersion=${gameVersion}&liveOnly=${liveOnly ? "true" : "false"}`
+      );
+      const data = await response.json();
+      const res = (await apiToPace(data)).sort(paceSort);
+      return res;
+    },
+    staleTime: 5000,
+    refetchInterval: 5000,
+    enabled: isFocusedOnHomePage,
+  });
+};
+
+export const useLiverunsDataSuspense = ({ gameVersion, liveOnly }: PaceSettings) => {
+  return useSuspenseQuery<Pace[]>({
     queryKey: ["liveruns", gameVersion, liveOnly],
     queryFn: async () => {
       const response = await fetch(
