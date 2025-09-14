b951da7f27e4aaf5763745c3e403582f49a7d467
chitrakshtarun <65770420+chitrakshtarun@users.noreply.github.com>
2025-09-14 19:16:47 +0530

    refactor(ui): remove headerBlurEffect on settings and player profiles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -60,7 +60,6 @@ export default function RootLayout() {
                     android: backgroundColor,
                   }),
                 },
-                headerBlurEffect: colorScheme === "light" ? "systemChromeMaterialLight" : "systemChromeMaterialDark",
               }}
             >
               <Stack.Screen
